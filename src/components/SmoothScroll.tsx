927a392e966eee2d05d3b8691764c88c4fddf527
parasmottan <parasmottan9316@gmail.com>
2026-03-15 16:59:29 +0530

    solve

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -7,34 +7,35 @@ interface SmoothScrollProps {
   children: ReactNode;
 }
 
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 export default function SmoothScroll({ children }: SmoothScrollProps) {
   useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      orientation: "vertical",
-      gestureOrientation: "vertical",
       smoothWheel: true,
-      wheelMultiplier: 1,
       // @ts-ignore
       smoothTouch: true,
-      touchMultiplier: 2,
-      infinite: false,
     } as any);
 
+    // Sync Lenis with GSAP ScrollTrigger
+    lenis.on("scroll", ScrollTrigger.update);
+
     function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
+      lenis.raf(time * 1000);
     }
 
-    requestAnimationFrame(raf);
+    gsap.ticker.add(raf);
+    gsap.ticker.lagSmoothing(0);
 
-    // Integrate with GSAP ScrollTrigger if used globally
-    // lenis.on("scroll", ScrollTrigger.update);
-    // gsap.ticker.add((time) => { lenis.raf(time * 1000); });
-    // gsap.ticker.lagSmoothing(0);
+    // Initial refresh
+    ScrollTrigger.refresh();
 
     return () => {
+      gsap.ticker.remove(raf);
       lenis.destroy();
     };
   }, []);
